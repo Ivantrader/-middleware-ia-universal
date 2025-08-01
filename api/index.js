020323de14a4e68ed7b9de693edf13e5e71dbb54
ivan trader <entulhogeral@gmail.com>
2025-07-31 21:53:34 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,14 +1,12 @@
-// api/index.js
 const express = require("express");
 const router = express.Router();
 
+// Rota GET em /api/
 router.get("/", (req, res) => {
   res.status(200).json({
     status: "Online",
-    message: "Middleware IA Universal funcionando com sucesso!"
+    message: "Middleware IA Universal funcionando com sucesso!",
   });
 });
-
-router.use("/status", require("./status"));
 
 module.exports = router;
